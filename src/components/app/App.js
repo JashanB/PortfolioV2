b5666cb4fbb38278f97f8ded6726d10ade7dc662
JashanB <jashan_brar50@hotmail.com>
2020-11-04 11:32:29 -0500

    bugfixing netlify deploy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Project from '../project';
 import Profile from '../profile';
