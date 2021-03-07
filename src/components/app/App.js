abc260523e0abaa474de45fb67561eeb8df53d99
JashanB <jashan_brar50@hotmail.com>
2021-03-07 16:52:04 -0500

    set margins and margin state setter

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,4 @@
-// import React, { useState } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../header';
 import Project from '../project';
@@ -8,20 +7,19 @@ import WorkExp from '../workExp';
 import Publications from '../publications';
 
 export default function App() {
-  // const [collapse, setCollapse] = useState(false); 
-  // const collapseMenu = () => {
-  //   collapse ? setCollapse(state => false) : setCollapse(state => true);
-  // };
+  const [expand, setExpand] = useState('content-wrapper');
 
   return (
     <div className="App">
-        <Header />
-        <div className="content-wrapper">
+      <Header setExpand={setExpand} />
+      <div className={expand}>
+        <div className="content-body">
           <Profile />
           <WorkExp />
           <Project />
           <Publications />
         </div>
       </div>
-      );
-    }
+    </div>
+  );
+}
